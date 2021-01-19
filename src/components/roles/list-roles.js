3a9a83ff914ee7274c7df9913cb8c5bf32aae1d2
rahimin <rahiminmyself@gmail.com>
2021-01-20 00:45:35 +0600

    update role status ui

--- src/components/roles/list-roles.js
+++ src/components/roles/list-roles.js
@@ -2,9 +2,32 @@ import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom'
 import Breadcrumb from '../common/breadcrumb';
 import Datatable from '../common/datatable';
+import Forms from "../form/forms";
+import {AvField} from "availity-reactstrap-validation";
+import {Button} from "reactstrap";
+import Modal from "react-responsive-modal";
 
 export default class ListRole extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            roleId:'',
+            open: false,
+            error: false,
+            errorMessage: null,
+        };
+    }
+
+    onOpenModal = () => {
+        this.setState({ open: true });
+    };
+
+    onCloseModal = () => {
+        this.setState({ open: false });
+    };
+
     render() {
+        let { open, roleId } = this.state;
         return (
             <Fragment>
                 <Breadcrumb title="Role List" parent="Users"/>
@@ -18,6 +41,33 @@ export default class ListRole extends Component {
                                 <Link to="/create-role" className="btn btn-secondary">Create Role</Link>
                             </div>
                             <div className="clearfix"></div>
+                            <Modal open={open} onClose={this.onCloseModal} center>
+                                <div className="modal-header">
+                                    <h5 className="modal-title f-w-600" id="exampleModalLabel2">Change Status</h5>
+                                </div>
+                                <div className="modal-body">
+                                    <Forms
+                                        options={{
+                                            method: 'PUT',
+                                            url: `/roles/${roleId}/change-status`,
+                                            onSuccess: (response) => {
+                                                window.location.href = '/list-roles';
+                                            },
+                                        }}
+                                    >
+                                        <AvField type="select" name="roles" id={"roles_"+userId} value={userRoles} label="Roles" helpMessage="Choose a Role to assign!" multiple>
+                                            {roleList.map(role => (
+                                                <option value={role.id}>{ role.Name }</option>
+                                            ))}
+                                        </AvField>
+
+                                        <Button className="btn btn-sm btn-secondary">Assign</Button>
+                                    </Forms>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-sm btn-d" onClick={() => this.onCloseModal('VaryingMdo')}>Close</button>
+                                </div>
+                            </Modal>
                             <div id="batchDelete" className="category-table user-list order-table coupon-list-delete">
                                 <Datatable
                                     url="/api/roles"
@@ -56,7 +106,10 @@ export default class ListRole extends Component {
                                                     </span>
 
                                                     <span onClick={() => {
-                                                        window.location.href = `/roles/${row.original.id}`;
+                                                        this.setState({
+                                                            roleId: row.original.id
+                                                        })
+                                                        this.onOpenModal();
                                                     }} title="Change Status">
                                                         <button className={"btn btn-xs " + (row.original.isActive ? 'btn-success' : 'btn-warning') }>{ row.original.isActive ? 'Active' : 'Inactive' }</button>
                                                     </span>
