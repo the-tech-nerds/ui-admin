347a0358c07f3fbf4d7b8f333ffddb07bb4b2a1f
arifulcuet52@gmail.com <arifulcuet52@gmail.com>
2021-03-23 01:39:02 +0600

    shop user add to user create

--- src/components/users/create-user.js
+++ src/components/users/create-user.js
@@ -3,10 +3,39 @@ import Breadcrumb from '../common/breadcrumb';
 import App from "../app";
 import Forms from "../form/forms";
 import {AvField} from "availity-reactstrap-validation";
-import {Button} from "reactstrap";
-
+import {Button, Label} from "reactstrap";
+import FetchData from "../common/get-data";
+import AvGroup from 'availity-reactstrap-validation/lib/AvGroup';
+import AvSelect from '@availity/reactstrap-validation-select';
 export class Create_user extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            shops: []
+        }
+    }
+    async componentDidMount() {
+        const id = Number(this.props.match.params.id);
+        // fetch shops
+        FetchData({
+            url: '/api/shops/list/all', callback: (response, isSucess) => {
+                if (isSucess) {
+                    const options = response.data.map(x => {
+                        return {
+                            label: x.Name,
+                            value: x.id
+                        };
+                    });
+                    this.setState({
+                        shops: options
+                    })
+                } 
+            }
+        })
+    }
+
     render() {
+        const {shops} = this.state;
         return (
             <App>
                 <Breadcrumb title="Create Admin" parent="Users" />
@@ -32,6 +61,10 @@ export class Create_user extends Component {
                                         <AvField name="email" label="Email" type="email" required />
                                         {/*<AvField name="password" label="Password" type="password" required />*/}
                                         <AvField name="phone" label="Phone" type="phone" required />
+                                        <AvGroup>
+                                                <Label for="shopId">Select Shop</Label>
+                                                <AvSelect isMulti name="shopId" options={shops} required/>
+                                        </AvGroup>
                                         <Button color="primary">Register</Button>
                                     </Forms>
                                 </div>
