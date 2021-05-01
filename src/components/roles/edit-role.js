c184f4339bda626fc39e0e9d27d9a5a0511a5256
ranasl62 <ranasl62@gmail.com>
2021-05-02 04:43:38 +0600

    Select all in permission module for edit role is done

--- src/components/roles/edit-role.js
+++ src/components/roles/edit-role.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react'
+import React, {Component} from 'react'
 import Breadcrumb from '../common/breadcrumb';
 import App from "../app";
 import Forms from "../form/forms";
@@ -18,7 +18,7 @@ export default class EditRole extends Component {
             selectedPermissions: [],
             selectedRole: {},
             role: '',
-            role_id:'',
+            role_id: '',
         }
     }
 
@@ -26,46 +26,6 @@ export default class EditRole extends Component {
         const url = window.location.href.split('/');
         const roleId = url[4];
         this.setState({loading: true});
-        await fetch(`/api/roles/${roleId}/permissions`, {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            cache: 'no-cache',
-            redirect: 'follow',
-        })
-            .then(async res => {
-                this.setState({loading: false});
-                const response = await res.json();
-                if (response.code === 200) {
-                    this.setState((state) => {
-                        return {
-                            ...state,
-                            selectedPermissions: response.data.permissions,
-                            selectedRole: response.data.role
-                        }
-                    });
-                } else {
-                    this.setState((state) => {
-                        return {
-                            ...state,
-                            error: true,
-                            errorMessage: response.message,
-                            loading: false,
-                        }
-                    });
-                }
-            })
-            .catch(error => {
-                this.setState((state) => {
-                    return {
-                        ...state,
-                        error: true,
-                        errorMessage: error,
-                        loading: false,
-                    }
-                });
-            });
         await fetch(`/api/permission-categories`, {
             method: "GET",
             headers: {
@@ -78,7 +38,6 @@ export default class EditRole extends Component {
                 this.setState({loading: false});
                 const response = await res.json();
                 if (response.code === 200) {
-                    // const categories =
                     this.setState((state) => {
                         return {
                             ...state,
@@ -104,14 +63,81 @@ export default class EditRole extends Component {
                     loading: false,
                 })
             });
-
+        await fetch(`/api/roles/${roleId}/permissions`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            cache: 'no-cache',
+            redirect: 'follow',
+        })
+            .then(async res => {
+                this.setState({loading: false});
+                const response = await res.json();
+                if (response.code === 200) {
+                    console.log(response);
+                    this.setState((state) => {
+                        const categories = this.state?.categories?.map((category) => {
+                            category.permissions = category.permissions?.map((permission) => {
+                                permission.checked = response?.data?.permissions?.includes(permission.id);
+                                return permission;
+                            });
+                            category.checked = category?.permissions?.findIndex((permission) => !permission.checked) === -1;
+                            return category;
+                        });
+                        return {
+                            ...state,
+                            categories,
+                            selectedRole: response.data.role
+                        }
+                    });
+                    console.log(this.state.categories);
+                } else {
+                    this.setState((state) => {
+                        return {
+                            ...state,
+                            error: true,
+                            errorMessage: response.message,
+                            loading: false,
+                        }
+                    });
+                }
+            })
+            .catch(error => {
+                this.setState((state) => {
+                    return {
+                        ...state,
+                        error: true,
+                        errorMessage: error,
+                        loading: false,
+                    }
+                });
+            });
     }
 
-    selectPermissions = (category) => {
-        const {selectedPermissions} = this.state;
-        return category.permissions.filter(permission => {
-            return selectedPermissions.includes(permission.id);
-        }).map(permission => permission.id);
+    checkPermission(category_index, permission_index) {
+        const categories = [...this.state.categories || []];
+        categories[category_index].permissions[permission_index].checked = !categories[category_index]?.permissions[permission_index]?.checked;
+        const categoryChecked = categories[category_index].permissions.findIndex((permission) => !permission.checked);
+        categories[category_index].checked = categoryChecked === -1;
+        this.setState((prepState) => ({
+            ...prepState,
+            categories: categories
+        }));
+    }
+
+    selectAllPermission(category_index) {
+        const categories = [...this.state.categories || []];
+        const categoryChecked = categories[category_index]?.checked;
+        categories[category_index].permissions = categories[category_index]?.permissions?.map((permission) => {
+            permission.checked = !categoryChecked;
+            return permission;
+        });
+        categories[category_index].checked = !categoryChecked;
+        this.setState((prepState) => ({
+            ...prepState,
+            categories: categories
+        }));
     }
 
     render() {
@@ -139,10 +165,9 @@ export default class EditRole extends Component {
                                             },
                                             dataProcessBeforeSubmit: (value, callback) => {
                                                 callback({
-                                                    permissions: Object.keys(value)
-                                                        .filter(pk => pk !== 'name')
-                                                        .reduce((allPermission, permissionKey) => [...allPermission, ...value[permissionKey]], []),
-                                                    name: value.name,
+                                                    permissions: this.state?.categories?.map((category) => category?.permissions?.filter((permission) => permission.checked) || [])
+                                                        .reduce((allPermission, permissions) => [...allPermission, ...permissions?.map((permission) => permission.id)], []),
+                                                    name: value.name
                                                 });
                                             },
                                         }}
@@ -166,8 +191,16 @@ export default class EditRole extends Component {
                                                                     <AvCheckboxGroup
                                                                         name={'permissions_' + i}
                                                                         id={"cat_" + category.id}
-                                                                        defaultValue={this.selectPermissions(category)}
                                                                     >
+                                                                        <AvCheckbox
+                                                                            label="Select all"
+                                                                            value={category.id}
+                                                                            className="checkbox_animated"
+                                                                            id={"perm_select" + category.id}
+                                                                            key={"perm_select" + category.id}
+                                                                            checked={category.checked}
+                                                                            onChange={() => this.selectAllPermission(i)}
+                                                                        />
                                                                         {category?.permissions?.map((permission, j) => (
                                                                             <AvCheckbox
                                                                                 label={permission.name}
@@ -175,6 +208,8 @@ export default class EditRole extends Component {
                                                                                 className="checkbox_animated"
                                                                                 id={"perm_" + permission.id}
                                                                                 key={"perm_" + permission.id}
+                                                                                checked={permission.checked}
+                                                                                onChange={() => this.checkPermission(i, j)}
                                                                             />
                                                                         ))}
                                                                     </AvCheckboxGroup>
